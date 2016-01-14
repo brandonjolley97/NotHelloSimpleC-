579e30f56b66ae85d2c57fdd1b3a32cbc0803459
brandonjolley97 <brandonjolley97@gmail.com>
2016-01-14 09:53:13 -0700

    Created a stupid project in C++

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,20 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: bjol7457
+ */
+#include <iostream> //Using input and output streams.
+using namespace std; //Access to the Standard Library
+
+int main()
+{
+	cout << "This is how you type to the console in C++" << endl;
+	//This would be like System.out.println("asdfghasg");
+
+
+	return 0;
+}
+
+
+
